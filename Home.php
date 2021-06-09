df89b7b0aa5c1be4f6d1013912d1894c5598fa88
MacBook <macbook@MacBooks-MacBook-Pro-55.local>
2021-06-09 17:36:39 -0400

    Code the basic functionality of the login form #15

---
+++ Home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style = "color: blue; font-size: 11pt; font-family: Tahoma; font-weight: normal; position: fixed; left: 1%; top: 1%;">
+            University at Buffalo Polling and Feedback System 
+        </h1>
+        
+        <h2 style = "font-size: 15pt; font-family: Tahoma; font-weight: normal; text-align: center; margin-top: 24%;">
+            Welcome, <?php echo $_POST["name"]; ?>!
+        </h2>
+    </body>
+</html>
