c2f9c453ff248b146d3d5660d43e399f3b30923e
Hon <47615001+honching0316@users.noreply.github.com>
2021-06-09 23:57:47 -0400

    see polling result #12
    Professor decide whenever the poll has ended

---
+++ www/init.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Are you a student or professor</h3>
+    <button onclick="location.href='prof_directPage/prof.php'">Professor</button>
+    <button onclick="location.href='form.php'">Student</button>
+</body>
+</html>
---
+++ www/prof_directPage/prof_result.php
@@ -0,0 +1,62 @@
+<?php
+
+$db_host = "localhost";
+$db_name = "June10th";
+$db_user="hon";
+$db_pass="123";
+
+$r = array();
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()){
+    echo mysqli_connect_error();
+    exit;
+}
+
+echo "<h3>Now the poll ended, displaying the result</h3>";
+
+// Once user get into this page, the start_poll result row will delete and re-insert
+$sql_delete = "DELETE FROM comments WHERE title='start_poll'";
+
+$query_res = mysqli_query($conn, $sql_delete);
+
+if ($query_res ==false){
+    echo mysqli_error($conn);
+}else{
+}
+
+$sql_insert = "INSERT INTO comments(title, comment)
+VALUES('" . "start_poll" . "','" . "no" . "')";
+$query_insert_res = mysqli_query($conn, $sql_insert);
+if($query_insert_res==false){
+    echo mysqli_error($conn);
+}else{
+}
+
+
+$sql_query = "SELECT * FROM comments";
+
+$query_res = mysqli_query($conn, $sql_query);
+
+if ($query_res ==false){
+    echo mysqli_error($conn);
+}else{
+    $res = mysqli_fetch_all($query_res);  // return array from result set from the db
+    $r = $res;
+    // var_dump($r);
+}
+
+
+foreach($r as $elem){
+    foreach($elem as $index => $val){
+        if ($index==0 && $val!=""){
+            echo "<br><h3>" . $val . ": ";
+        } elseif ($index==1 && $val!=""){
+            echo $val ."</h3>";
+        }
+        
+    }
+}
+
+?>
