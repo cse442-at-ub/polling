ad213e99232d8b5023fef2b0ad2d9b1eab6f2733
honching0316 <honching@buffalo.edu>
2021-06-13 00:44:00 -0400

    write function for connection to DB #20

--- hon_www/prof_directPage/prof_result.php
+++ hon_www/prof_directPage/prof_result.php
@@ -5,14 +5,26 @@ $db_name = "honching_db";
 $db_user="honching";
 $db_pass="50185646";
 
-$r = array();
+// $db_host = "localhost";
+// $db_name = "June10th";
+// $db_user="hon";
+// $db_pass="123";
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if (mysqli_connect_error()){
-    echo mysqli_connect_error();
-    exit;
+// this function return a connection to the DB
+function connect_db($host, $user, $pass, $name){
+    $connection = mysqli_connect($host, $user, $pass, $name);
+    if (mysqli_connect_error()){
+        echo mysqli_connect_error();
+        exit;
+    }
+    echo "<h3>Student</h3>";
+    echo "Connected successfully.";
+    return $connection;
 }
+
+$conn = connect_db($db_host, $db_user, $db_pass, $db_name);
+
+$r = array();
 
 echo "<h3>Now the poll ended, displaying the result</h3>";
 
--- hon_www/see_result.php
+++ hon_www/see_result.php
@@ -5,20 +5,30 @@ $db_name = "honching_db";
 $db_user="honching";
 $db_pass="50185646";
 
-$r = array();
+// $db_host = "localhost";
+// $db_name = "June10th";
+// $db_user="hon";
+// $db_pass="123";
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if (mysqli_connect_error()){
-    echo mysqli_connect_error();
-    exit;
+// this function return a connection to the DB
+function connect_db($host, $user, $pass, $name){
+    $connection = mysqli_connect($host, $user, $pass, $name);
+    if (mysqli_connect_error()){
+        echo mysqli_connect_error();
+        exit;
+    }
+    echo "<h3>Student</h3>";
+    echo "Connected successfully.";
+    return $connection;
 }
 
-echo "Connection okay";
+$conn = connect_db($db_host, $db_user, $db_pass, $db_name);
 
 $sql_query = "SELECT * FROM comments WHERE title='start_poll'";
 
 $query_res = mysqli_query($conn, $sql_query);
+
+$r = array();
 
 if ($query_res ==false){
     echo mysqli_error($conn);
