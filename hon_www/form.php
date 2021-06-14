d3fe35cf205c16cc66a1e3a88bd2266ab9fdfe11
honching0316 <honching@buffalo.edu>
2021-06-14 11:36:21 -0400

    extract repeat connection code to connect_db.php #20

--- hon_www/form.php
+++ hon_www/form.php
@@ -1,28 +1,6 @@
 <?php
 
-// $db_host = "localhost";
-// $db_name = "June10th";
-// $db_user="hon";
-// $db_pass="123";
-
-$db_host = "oceanus.cse.buffalo.edu:3306";
-$db_name = "honching_db";
-$db_user="honching";
-$db_pass="50185646";
-
-// this function return a connection to the DB
-function connect_db($host, $user, $pass, $name){
-    $connection = mysqli_connect($host, $user, $pass, $name);
-    if (mysqli_connect_error()){
-        echo mysqli_connect_error();
-        exit;
-    }
-    echo "<h3>Student</h3>";
-    echo "Connected successfully.";
-    return $connection;
-}
-
-$conn = connect_db($db_host, $db_user, $db_pass, $db_name);
+require 'connect_db.php';
 
 $sql_query = "SELECT * FROM comments";
 
