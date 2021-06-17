965089e6d9cb8cdc09b4084526a7f5f74635949d
yli55 <32351796+landonlane72@users.noreply.github.com>
2021-06-17 10:47:00 -0400

    hardcode the UBIT as variable, No more sign in with Name or UBIT for #21

--- feedBack_timeStamp.php
+++ feedBack_timeStamp.php
@@ -12,7 +12,7 @@
 <body>
 
   <?php
-  /*
+
   $db_host = "oceanus.cse.buffalo.edu:3306";
   $db_name = "cse442_2021_summer_team_b_db"; //honching_db yli55_db is not correct, here is our summer database name
   $db_user = "yli55"; //honching
@@ -36,7 +36,7 @@
   }
 
   $conn = connect_db($db_host, $db_user, $db_pass, $db_name);
-*/
+
 
   //-- database above---------------------------------------------
   // define variables and set to empty values
@@ -45,8 +45,12 @@
   $flag = '';
   $update = '';
   $currentTime = '';
+  // hardcoded
+  $UBIT = 'yli55';
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $flag = 'submitted';
+
+    /*
     if (empty($_POST["name"])) {
       $nameErr = "Name is required";
     } else {
@@ -55,8 +59,9 @@
       if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
         $nameErr = "Only letters and white space allowed";
       }
-    }
+    } */
 
+    /*
     if (empty($_POST["UBIT"])) {
       $UBITErr = "UBIT is required";
     } else {
@@ -65,7 +70,7 @@
       if (!preg_match("/^[a-zA-Z0-9]*$/", $UBIT)) {
         $UBITErr = "Invalid UBIT format";
       }
-    }
+    }  */
 
     if (empty($_POST["feadBack"])) {
       $feadBackErr = "feedback is required";
@@ -147,7 +152,9 @@
       date_default_timezone_set('America/New_York');
       $currentTime = date("Y-m-d H:i:s");
       //echo ($currentTime);
-      $sql_insert = "INSERT INTO feedBackTable(InputTime,UBIT,Feedback) VALUES('" . $currentTime . "' ,'" . $_POST['UBIT'] . "','" . $_POST['feadBack'] . "')";
+      //$sql_insert = "INSERT INTO feedBackTable(InputTime,UBIT,Feedback) VALUES('" . $currentTime . "' ,'" . $_POST['UBIT'] . "','" . $_POST['feadBack'] . "')";
+      $sql_insert = "INSERT INTO feedBackTable(InputTime,UBIT,Feedback) VALUES('" . $currentTime . "' ,'" . $UBIT . "','" . $_POST['feadBack'] . "')";
+
       //$sql_insert = "INSERT INTO feedBackTable(UBIT,Feedback) VALUES('yli55','I am lost')";// this one works //$currentTime
 
       $query_insert_res = mysqli_query($conn, $sql_insert);
@@ -180,13 +187,17 @@
   <h2>Feedback Form</h2>
   <p><span class="error">* required field</span></p>
   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    Name: <input type="text" name="name" value="<?php echo $name; ?>">
-    <span class="error">* <?php echo $nameErr; ?></span>
+    <!--
+    Name: <input type="text" name="name" value="<?php //echo $name; 
+                                                ?>">
+    <span class="error">* <?php //echo $nameErr; 
+                          ?></span>
     <br><br>
     UBIT: <input type="text" name="UBIT" value="<?php echo $UBIT; ?>">
-    <span class="error">* <?php echo $UBITErr; ?></span>
+    <span class="error">* <?php //echo $UBITErr; 
+                          ?></span>
     <br><br>
-    <br><br>
+    <br><br>  -->
     Feedback:
     <br><br>
     How do you understand the content so far?
@@ -214,7 +225,7 @@
       // }
       //echo nl2br("87  '" . $_POST['UBIT'] . "' \r\n");
       echo "<h2>Your input recorded below:</h2>";
-      echo nl2br("Your UBIT:  '" . $_POST['UBIT'] . "' \r\n");
+      echo nl2br("Your UBIT:  '" . $UBIT . "' \r\n");
       echo "<br>";
       echo nl2br("Your Feedback:  '" . $_POST['feadBack'] . "' \r\n");
       echo "<br>";
